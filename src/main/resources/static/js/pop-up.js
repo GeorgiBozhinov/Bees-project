de0123f15dc838d7fc8c167f5b3f1f07b8830742
GeorgiBozhinov <g_bozhinov@outlook.com>
2020-08-30 19:43:18 +0300

    Yesterday I did some changes on localhost:8080/profile file.

--- src/main/resources/static/js/pop-up.js
+++ src/main/resources/static/js/pop-up.js
@@ -1,7 +1,45 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+function openForm(id) {
+
+    // 0 -> Обща информация
+    if (id === 0) {
+        // You could replace it with FOR loop
+        document.getElementById("myForm").style.display = "block";
+        document.getElementById("bee-farm").style.display = "none";
+        document.getElementById("my-publications").style.display = "none";
+        document.getElementById("my-comment").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+    // 1 -> Моите пчелини
+    } else if (id === 1) {
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("my-publications").style.display = "none";
+        document.getElementById("my-comment").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+        document.getElementById("bee-farm").style.display = "block";
+    // 2 -> Моите теми (публикации)
+    } else if (id === 2) {
+        document.getElementById("my-publications").style.display = "block";
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("bee-farm").style.display = "none";
+        document.getElementById("my-comment").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+    // 3 -> Моите коментари
+    } else if (id === 3) {
+        document.getElementById("my-publications").style.display = "none";
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("bee-farm").style.display = "none";
+        document.getElementById("my-comment").style.display = "block";
+        document.getElementById("settings").style.display = "none";
+    // 4 -> Настройки
+    } else if (id === 4) {
+        document.getElementById("my-publications").style.display = "none";
+        document.getElementById("myForm").style.display = "none";
+        document.getElementById("bee-farm").style.display = "none";
+        document.getElementById("my-comment").style.display = "none";
+        document.getElementById("settings").style.display = "block";
+    }
 }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+// Not in use
+// function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+// }
